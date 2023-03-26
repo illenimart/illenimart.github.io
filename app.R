ca5e11ba2b0d5fa8078fa0435a57606e544346a4
illenimart <illenimart@gmail.com>
2023-03-25 23:38:54 -0400

    add fun rshiny app

---
+++ app.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(tibble)
+
+# Song data
+mg_songs <- data.frame(id = 1:2,
+                       song = c("me cri", "me cri2"),
+                       url = c("https://youtu.be/o3YadwGH0ZA?t=1962", "https://youtu.be/o3YadwGH0ZA?t=3819"))
+il_songs <- data.frame(id = 1:2,
+                       song = c("sadboi1", "sadboi2"),
+                       url = c("https://www.youtube.com/watch?v=fJVoozJ7IPE&ab_channel=ILLENIUM", "https://youtu.be/E1ZCQqQhRTU?t=2823"))
+# UI
+ui <- fluidPage(
+  titlePanel("Random Song URL"),
+  sidebarLayout(
+    sidebarPanel(
+      br(),
+      selectInput("artist", "Choose an artist:", choices = c("Martin Garrix", "Illenium")),
+      br(),
+      actionButton("generate", "Generate URL")
+    ),
+    mainPanel(
+      h4(textOutput("title")),
+      br(),
+      uiOutput("tab")
+    )
+  )
+)
+
+# Server
+server <- function(input, output, session) {
+  # Function to generate a random song URL
+  generate_url <- function(artist) {
+    if (artist == "Martin Garrix") {
+      id <- sample(mg_songs$id, 1)
+      song <- mg_songs$song[id]
+      url <- a("surprise", href=mg_songs$url[id])
+    } else if (artist == "Illenium") {
+      id <- sample(il_songs$id, 1)
+      song <- il_songs$song[id]
+      url <- a("surprise", href=il_songs$url[id])
+    }
+    return(list(song = song, url = url))
+  }
+  
+  # Generate URL on button click
+  observeEvent(input$generate, {
+    result <- generate_url(input$artist)
+    output$title <- renderText(paste0("Song Title: ", result$song))
+    output$tab <- renderUI({
+      tagList("URL link:", result$url)
+    })
+  })
+}
+
+# Run app
+shinyApp(ui, server)
